bc003cd50035c73b83b8d12d39ca7c5e66543a72
Akanksha-creat <137200256+Akanksha-creat@users.noreply.github.com>
2026-04-06 15:17:08 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>📖 About Food Wastage</h1>
+</header>
+
+<section>
+    <p>Food wastage is a serious global issue.</p>
+
+    <div class="card-container">
+        <div class="card">🌍 1/3 food wasted globally</div>
+        <div class="card">💧 Wastes water & energy</div>
+        <div class="card">🌫 Increases pollution</div>
+    </div>
+</section>
+
+</body>
+</html>
